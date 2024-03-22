c0ff15d4b88b30a825e3dc4111b5819b9533a236
fukang1163 <fukang1163@cairenhui.com>
2024-03-22 11:32:31 +0800

    20240322 HttpClient注解添加@Inherited注解

--- lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/annotation/HttpClient.java
+++ lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/annotation/HttpClient.java
@@ -5,6 +5,7 @@ import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -18,6 +19,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @DomainName
 @HttpClientComponent
 public @interface HttpClient {
