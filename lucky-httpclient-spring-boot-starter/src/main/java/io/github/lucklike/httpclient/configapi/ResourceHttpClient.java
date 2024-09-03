2e66321d2019ecb1b5d600e2849897b13f388de4
fukang1163 <fukang1163@cairenhui.com>
2024-09-03 10:46:43 +0800

    20240903 @LocalConfigHttpClient注解更名为@ResourceHttpClient

--- lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/configapi/ResourceHttpClient.java
+++ lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/configapi/ResourceHttpClient.java
@@ -17,7 +17,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static com.luckyframework.httpclient.proxy.configapi.Source.LOCAL_FILE;
+import static com.luckyframework.httpclient.proxy.configapi.Source.RESOURCE;
+
 
 /**
  *
@@ -441,7 +442,7 @@ import static com.luckyframework.httpclient.proxy.configapi.Source.LOCAL_FILE;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @HttpClientComponent
-@EnableConfigurationParser(sourceType = LOCAL_FILE)
+@EnableConfigurationParser(sourceType = RESOURCE)
 @Combination({EnableConfigurationParser.class})
 public @interface ResourceHttpClient {
 
