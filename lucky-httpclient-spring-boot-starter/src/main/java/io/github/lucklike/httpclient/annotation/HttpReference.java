97d6e8c02055660c669bdb7afcbd0850272dfb5f
fukang1163 <fukang1163@cairenhui.com>
2024-10-09 15:05:52 +0800

    20241009 注释优化

--- lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/annotation/HttpReference.java
+++ lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/annotation/HttpReference.java
@@ -6,6 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 用于支持属性/方法注入的注解
+ *
+ * @author fukang
+ * @version 1.0.0
+ * @date 2024/10/09 14:50
+ */
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
