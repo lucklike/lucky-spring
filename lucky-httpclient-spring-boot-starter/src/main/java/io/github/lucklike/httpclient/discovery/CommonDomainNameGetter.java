6cfd7657c9f0c557bcdf6d287ca50b4686c722a3
fk7075 <1814375626@qq.com>
2025-03-13 23:39:46 +0800

    20250313 报错提示优化

--- lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/discovery/CommonDomainNameGetter.java
+++ lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/discovery/CommonDomainNameGetter.java
@@ -5,6 +5,7 @@ import com.luckyframework.httpclient.proxy.annotations.DomainNameMeta;
 import com.luckyframework.httpclient.proxy.url.DomainNameContext;
 import com.luckyframework.httpclient.proxy.url.DomainNameGetter;
 import io.github.lucklike.httpclient.ApplicationContextUtils;
+import io.github.lucklike.httpclient.discovery.cloud.SpringCloudDomainNameGetter;
 
 import static io.github.lucklike.httpclient.discovery.Constant.SPRING_CLOUD_DOMAIN_GETTER_BEAN_NAME;
 
@@ -21,18 +22,28 @@ public class CommonDomainNameGetter implements DomainNameGetter {
     public String getDomainName(DomainNameContext context) throws Exception {
         HttpClient httpClientAnn = context.toAnnotation(HttpClient.class);
 
+
+        String url = context.parseExpression(httpClientAnn.url(), String.class);
+        String serviceName = context.parseExpression(httpClientAnn.service(), String.class);
+        String path = context.parseExpression(httpClientAnn.path(), String.class);
+
         // 存在url配置时优先使用url配置
-        if (StringUtils.hasText(httpClientAnn.url())) {
-            String url = context.parseExpression(httpClientAnn.url(), String.class);
-            String path = context.parseExpression(httpClientAnn.path(), String.class);
+        if (StringUtils.hasText(url)) {
             return StringUtils.joinUrlPath(url, path);
+        }
+
+        // url和service均为配置时报错提示
+        if (!StringUtils.hasText(serviceName)) {
+            throw new ServerDiscoveryConfigurationException("Missing url configuration and service name configuration");
         }
 
         // 尝试使用server配置进行解析，server解析需要依赖SpringCloud环境，如果不在SprigCloud环境时将无法解析，会直接返回空字符串
         if (!ApplicationContextUtils.containsBean(SPRING_CLOUD_DOMAIN_GETTER_BEAN_NAME)) {
             return DomainNameMeta.EMPTY;
         }
-        return ApplicationContextUtils.getBean(SPRING_CLOUD_DOMAIN_GETTER_BEAN_NAME, DomainNameGetter.class).getDomainName(context);
+        return ApplicationContextUtils
+                .getBean(SPRING_CLOUD_DOMAIN_GETTER_BEAN_NAME, SpringCloudDomainNameGetter.class)
+                .getDomainName(serviceName, path);
     }
 
 }
