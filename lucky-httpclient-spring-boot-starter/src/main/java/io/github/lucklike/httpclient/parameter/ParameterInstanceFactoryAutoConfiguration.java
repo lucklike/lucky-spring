1ef6cab57bc244c6fa8851db5e661fd70eadc8d4
fukang1163 <fukang1163@cairenhui.com>
2025-06-12 14:03:05 +0800

    20250612 给内置的ParameterInstanceFactory加上顺序

--- lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/parameter/ParameterInstanceFactoryAutoConfiguration.java
+++ lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/parameter/ParameterInstanceFactoryAutoConfiguration.java
@@ -2,20 +2,24 @@ package io.github.lucklike.httpclient.parameter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class ParameterInstanceFactoryAutoConfiguration {
 
+    @Order(Integer.MIN_VALUE)
     @Bean("__httpReferenceParameterInstanceFactory__")
     public ParameterInstanceFactory httpReferenceParameterInstanceFactory() {
         return new HttpReferenceParameterInstanceFactory();
     }
 
+    @Order(Integer.MIN_VALUE + 1)
     @Bean("__qualifierParameterInstanceFactory__")
     public ParameterInstanceFactory qualifierParameterInstanceFactory() {
         return new QualifierParameterInstanceFactory();
     }
 
+    @Order(Integer.MIN_VALUE + 2)
     @Bean("__valueParameterInstanceFactory__")
     public ParameterInstanceFactory valueParameterInstanceFactory() {
         return new ValueParameterInstanceFactory();
