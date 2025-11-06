407936934094511be8cf34cc85a5be46995f04e2
fk7075 <1814375626@qq.com>
2025-11-07 02:57:37 +0800

    20251107 SpEL 表达式执行逻辑变更： 如果表达式是一个单一的环境变量表达式，即以'${'开头，以'}'结尾的表达式，则会先进行一次 SpEL 计算

--- lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/SpringSpELConvert.java
+++ lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/SpringSpELConvert.java
@@ -7,7 +7,10 @@ import org.springframework.core.env.Environment;
 
 /**
  * 提供持使用'${}'占位符从Spring环境变量中取值的功能，
- * ‘${}’表达式的优先级高于'#{}'表达式
+ * <pre>
+ *     1.如果表达式是一个单一的环境变量表达式，即以'${'开头，以'}'结尾的表达式，则会先进行一次 SpEL 计算
+ *     2.其他情况会优先执行‘${}’，再执行'#{}'
+ * </pre>
  *
  * @author fukang
  * @version 1.0.0
@@ -15,6 +18,19 @@ import org.springframework.core.env.Environment;
  */
 public class SpringSpELConvert extends SpELConvert {
 
+    /**
+     * 环境变量表达式前嘴
+     */
+    private final String ENV_EXPRESSION_PREFIX = "${";
+
+    /**
+     * 环境变量表达式前嘴
+     */
+    private final String ENV_EXPRESSION_SUFFIX = "}";
+
+    /**
+     * 环境变量
+     */
     private final Environment environment;
 
     public SpringSpELConvert(Environment environment) {
@@ -33,8 +49,12 @@ public class SpringSpELConvert extends SpELConvert {
 
     @Override
     protected void paramWrapperPostProcess(ParamWrapper paramWrapper) {
-        paramWrapper.setExpression(environment.resolveRequiredPlaceholders(paramWrapper.getExpression()));
         super.paramWrapperPostProcess(paramWrapper);
+        String expression = paramWrapper.getExpression();
+        if (isSingleEnvExpression(expression)) {
+            expression = getSpELRuntime().getValueForType(paramWrapper);
+        }
+        paramWrapper.setExpression(environment.resolveRequiredPlaceholders(expression));
     }
 
     @Override
@@ -53,6 +73,17 @@ public class SpringSpELConvert extends SpELConvert {
      * @return 否为环境变量取值表达式
      */
     protected boolean isEnvExpression(String text) {
-        return isExpression(text, "${", "}");
+        return isExpression(text, ENV_EXPRESSION_PREFIX, ENV_EXPRESSION_SUFFIX);
+    }
+
+    /**
+     * 判断是否为单一环境变量取值表达式，即以'${'开头，以'}'结尾的表达式？
+     *
+     * @param text 待判断的文本
+     * @return 否为单一环境变量取值表达式
+     */
+    private boolean isSingleEnvExpression(String text) {
+        String expression = text.trim();
+        return expression.startsWith(ENV_EXPRESSION_PREFIX) && expression.endsWith(ENV_EXPRESSION_SUFFIX);
     }
 }
