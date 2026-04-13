ba745f85a9020b80607f595d689bab5615d4219e
fukang1163 <fukang1163@cairenhui.com>
2026-04-13 16:59:32 +0800

    20260413 @InitBind注解支持SpEL表达式计算

--- lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/convert/InitBindParameterConvert.java
+++ lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/convert/InitBindParameterConvert.java
@@ -1,10 +1,20 @@
 package io.github.lucklike.httpclient.convert;
 
+import com.luckyframework.exception.LuckyReflectionException;
+import com.luckyframework.httpclient.core.util.BeanUtils;
+import com.luckyframework.httpclient.core.util.PropertyConvert;
+import com.luckyframework.httpclient.core.util.PropertyFilter;
+import com.luckyframework.httpclient.core.util.PropertyInfo;
+import com.luckyframework.httpclient.proxy.context.Context;
 import com.luckyframework.httpclient.proxy.context.ValueContext;
+import com.luckyframework.httpclient.proxy.function.CommonFunctions;
 import com.luckyframework.httpclient.proxy.unpack.ParameterConvert;
 import com.luckyframework.reflect.ClassUtils;
 import io.github.lucklike.httpclient.function.BeanFunction;
 import io.github.lucklike.httpclient.injection.BindException;
+
+import static com.luckyframework.httpclient.core.util.BeanUtils.copyProperties;
+import static io.github.lucklike.httpclient.function.BeanFunction.env;
 
 /**
  * 用于将环境变量中的某段配置绑定到某个参数上的转换类
@@ -55,7 +65,7 @@ public class InitBindParameterConvert implements ParameterConvert {
         for (String conf : initBind.value()) {
             try {
                 String propertyName = context.parseExpression(conf);
-                BeanFunction.initBind(value, propertyName);
+                initBind(context, value, propertyName);
             } catch (BindException e) {
                 if (!initBind.allowConfigNotExist()) {
                     throw e;
@@ -63,5 +73,64 @@ public class InitBindParameterConvert implements ParameterConvert {
             }
         }
         return value;
+    }
+
+    /**
+     * 初始化绑定
+     *
+     * @param context      上下文对象
+     * @param targetObject 真实对象
+     * @param prefix       配置前缀
+     */
+    public static void initBind(Context context, Object targetObject, String prefix) {
+        Object configObj = env(prefix, targetObject.getClass());
+        BeanUtils.TargetPropertyIsDefValueExecuteCopy filter = new BeanUtils.TargetPropertyIsDefValueExecuteCopy();
+        copyProperties(configObj, targetObject, filter, new SpELPropertyConvert(context, filter));
+    }
+
+    /**
+     * 默认的属性转换器
+     */
+    static class SpELPropertyConvert implements PropertyConvert {
+
+        private final PropertyFilter filter;
+        private final Context context;
+
+        SpELPropertyConvert(Context context, PropertyFilter filter) {
+            this.filter = filter;
+            this.context = context;
+        }
+
+
+        @Override
+        public void convert(PropertyInfo sourceProperty, PropertyInfo targetProperty) {
+            // 进行SpEL计算
+            Object propertyValue = sourceProperty.getValue();
+            if (propertyValue instanceof String) {
+                propertyValue = context.parseExpression(propertyValue.toString(), String.class);
+            }
+
+            if (sourceProperty.isJdkType()) {
+                targetProperty.setValue(propertyValue);
+            } else {
+
+                //目标对象的属性不为null时，直接进行属性的拷贝
+                if (propertyValue != null) {
+                    copyProperties(sourceProperty.getValue(), propertyValue, filter, this);
+                }
+                // 目标对象的属性为null时，尝试使用反射调用其无参构造器进行构造之后再进行属性的拷贝
+                else {
+                    try {
+                        Object newTargetPropertyValue = ClassUtils.newObject(targetProperty.getDescriptor().getPropertyType());
+                        copyProperties(sourceProperty.getValue(), newTargetPropertyValue, filter, this);
+                        targetProperty.setValue(newTargetPropertyValue);
+                    } catch (LuckyReflectionException e) {
+                        // ignore
+                    }
+                }
+
+
+            }
+        }
     }
 }
