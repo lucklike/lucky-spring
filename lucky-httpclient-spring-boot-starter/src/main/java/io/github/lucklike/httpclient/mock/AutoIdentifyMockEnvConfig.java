6cd34a7632c26482ef7e5cd878e59b8f9013bfaf
fukang1163 <fukang1163@cairenhui.com>
2026-05-06 14:12:45 +0800

    20260506 统一Mock配置结构

--- lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/mock/AutoIdentifyMockEnvConfig.java
+++ lucky-httpclient-spring-boot-starter/src/main/java/io/github/lucklike/httpclient/mock/AutoIdentifyMockEnvConfig.java
@@ -1,12 +1,12 @@
 package io.github.lucklike.httpclient.mock;
 
-import com.luckyframework.common.ContainerUtils;
-import com.luckyframework.common.Resources;
 import com.luckyframework.common.StringUtils;
 import com.luckyframework.httpclient.proxy.context.ClassContext;
 import com.luckyframework.httpclient.proxy.context.MethodContext;
 import com.luckyframework.httpclient.proxy.mock.Mock;
 import com.luckyframework.httpclient.proxy.mock.MockResponse;
+import com.luckyframework.httpclient.proxy.mock.config.MockConfigFunction;
+import com.luckyframework.httpclient.proxy.mock.config.MockConfiguration;
 import com.luckyframework.httpclient.proxy.spel.FunctionAlias;
 import com.luckyframework.httpclient.proxy.spel.SpELImport;
 import com.luckyframework.httpclient.proxy.spel.hook.Lifecycle;
@@ -19,8 +19,6 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 自动识别环境变量中的Mock配置
@@ -34,7 +32,7 @@ import java.util.Map;
 @Documented
 @Inherited
 @Mock(enable = "#{__config_mock_enable__($mc$)}", mockResp = "#{__config_mock_result__($mc$)}")
-@SpELImport(AutoIdentifyMockEnvConfig.MockConfigFunction.class)
+@SpELImport(AutoIdentifyMockEnvConfig.MockConfigFunctionAndCallback.class)
 public @interface AutoIdentifyMockEnvConfig {
 
     /**
@@ -46,9 +44,9 @@ public @interface AutoIdentifyMockEnvConfig {
     /**
      * Mock配置相关的工具函数与回调函数
      */
-    class MockConfigFunction {
+    class MockConfigFunctionAndCallback {
 
-        public static final String USE_MOCK_CONFIG = "$useMockConfiguration";
+        public static final String MOCK_CONFIG = "$AutoIdentifyMockEnvConfig";
 
         /**
          * 初始化Mock配置，检查Mock配置是否存在，存在则加载
@@ -56,10 +54,10 @@ public @interface AutoIdentifyMockEnvConfig {
          * @param cc 类上下文对象
          * @return Mock配置
          */
-        @Callback(lifecycle = Lifecycle.CLASS, storeOrNot = true, storeName = USE_MOCK_CONFIG)
+        @Callback(lifecycle = Lifecycle.CLASS, storeOrNot = true, storeName = MOCK_CONFIG)
         public static MockConfiguration initMockConfiguration(ClassContext cc) {
             AutoIdentifyMockEnvConfig annConfig = cc.getMergedAnnotation(AutoIdentifyMockEnvConfig.class);
-            String configKey = annConfig.value();
+            String configKey = cc.parseExpression(annConfig.value(), String.class);
             if (!StringUtils.hasText(configKey)) {
                 return null;
             }
@@ -79,17 +77,8 @@ public @interface AutoIdentifyMockEnvConfig {
          */
         @FunctionAlias("__config_mock_enable__")
         public static boolean mockEnable(MethodContext mc) {
-            // 判断总开关
-            MockConfiguration mockConfig = mc.getRootVar(USE_MOCK_CONFIG, MockConfiguration.class);
-            if (mockConfig == null || !mockConfig.isEnable()) {
-                return false;
-            }
-
-            // 判断方法级别开关
-            Map<String, MockResult> methods = mockConfig.getMethods();
-            String methodName = mc.getCurrentAnnotatedElement().getName();
-            MockResult methodMock = methods.get(methodName);
-            return methodMock != null && methodMock.isEnable();
+            MockConfiguration mockConfig = mc.getRootVar(MOCK_CONFIG, MockConfiguration.class);
+            return MockConfigFunction.mockEnable(mc, mockConfig);
         }
 
 
@@ -102,142 +91,20 @@ public @interface AutoIdentifyMockEnvConfig {
          */
         @FunctionAlias("__config_mock_result__")
         public static MockResponse mockResult(MethodContext mc) throws InterruptedException {
-            AutoIdentifyMockEnvConfig mockConfigAnn = mc.getMergedAnnotationCheckParent(AutoIdentifyMockEnvConfig.class);
 
-            MockResponse mockResponse = MockResponse.create();
+            // 将Mock配置转化为MockResponse对象
+            MockConfiguration mockConfig = mc.getRootVar(MOCK_CONFIG, MockConfiguration.class);
+            MockResponse mockResponse = MockConfigFunction.mockResult(mc, mockConfig);
+
+            // 设置特殊Mock响应头
+            AutoIdentifyMockEnvConfig mockConfigAnn = mc.getMergedAnnotationCheckParent(AutoIdentifyMockEnvConfig.class);
             mockResponse.header("Mock-Annotation", "@AutoIdentifyMockEnvConfig");
             mockResponse.header("Mock-Config-Env-Key", mockConfigAnn.value());
-
-            MockConfiguration mockConfig = mc.getRootVar(USE_MOCK_CONFIG, MockConfiguration.class);
-            String methodName = mc.getCurrentAnnotatedElement().getName();
-            MockResult mockResult = mockConfig.getMethods().get(methodName);
-
-            // main
-            Long latency = mockResult.getLatency() == null ? mockConfig.getLatency() : mockResult.getLatency();
-            Integer status = mockResult.getStatus();
-            Map<String, Object> headers = mockResult.getHeaders();
-
-            // match
-            boolean bodySetter = false;
-            List<WhenMockResult> matchList = mockResult.getMatch();
-            if (ContainerUtils.isNotEmptyCollection(matchList)) {
-                for (WhenMockResult math : matchList) {
-
-                    // 判断when表达式是否成立
-                    String whenExp = math.getWhen();
-                    if (!StringUtils.hasText(whenExp) || !mc.parseExpression(whenExp, boolean.class)) {
-                        continue;
-                    }
-
-                    mockResponse.header("Mock-Branch", whenExp);
-
-                    // latency
-                    latency = math.getLatency() == null ? latency : math.getLatency();
-
-                    // status
-                    status = math.getStatus() == null ? status : math.getStatus();
-
-                    // headers
-                    Map<String, Object> _headers = math.getHeaders();
-                    if (ContainerUtils.isNotEmptyMap(headers)) {
-                        if (ContainerUtils.isNotEmptyMap(_headers)) {
-                            headers.putAll(_headers);
-                        }
-                    } else {
-                        headers = _headers;
-                    }
-
-                    // body
-                    MockBody body = math.getBody();
-                    // file
-                    if (StringUtils.hasText(body.getFile())) {
-                        mockResponse.resource(Resources.getResource(mc.parseExpression(body.getFile(), String.class)));
-                        bodySetter = true;
-                    }
-
-                    // txt
-                    if (!bodySetter) {
-                        if (StringUtils.hasText(body.getTxt())) {
-                            mockResponse.body(mc.parseExpression(body.getTxt(), String.class));
-                            bodySetter = true;
-                        }
-                    }
-                    break;
-                }
-            }
-
-            // status
-            setStatus(mockResponse, status);
-
-            // header
-            setHeaders(mc, mockResponse, headers);
-
-            // body
-            MockBody body = mockResult.getBody();
-            if (!bodySetter) {
-                if (StringUtils.hasText(body.getFile())) {
-                    mockResponse.resource(Resources.getResource(mc.parseExpression(body.getFile(), String.class)));
-                    bodySetter = true;
-                }
-
-                // TXT
-                if (!bodySetter) {
-                    if (StringUtils.hasText(body.getTxt())) {
-                        mockResponse.body(mc.parseExpression(body.getTxt(), String.class));
-                    }
-                }
-            }
-
-            //latency
-            setLatency(latency);
 
             //return
             return mockResponse;
         }
 
-        /**
-         * 设置状态
-         *
-         * @param mockResponse Mock响应
-         * @param status       状态配置
-         */
-        private static void setStatus(MockResponse mockResponse, Integer status) {
-            mockResponse.status(status == null ? 200 : status);
-        }
-
-        /**
-         * 设置延时
-         *
-         * @param latency 延时配置
-         * @throws InterruptedException 可能出现的异常
-         */
-        private static void setLatency(Long latency) throws InterruptedException {
-            if (latency != null && latency > 0) {
-                Thread.sleep(latency);
-            }
-        }
-
-        /**
-         * 设置响应头
-         *
-         * @param mc           方法上下文
-         * @param mockResponse Mock响应
-         * @param headers      响应头配置
-         */
-        private static void setHeaders(MethodContext mc, MockResponse mockResponse, Map<String, Object> headers) {
-            if (ContainerUtils.isNotEmptyMap(headers)) {
-                headers.forEach((k, v) -> {
-                    String hName = mc.parseExpression(k, String.class);
-                    if (ContainerUtils.isIterable(v)) {
-                        ContainerUtils.getIterable(v).forEach(e -> {
-                            mockResponse.header(hName, mc.parseExpression(String.valueOf(e)));
-                        });
-                    } else {
-                        mockResponse.header(hName, mc.parseExpression(String.valueOf(v)));
-                    }
-                });
-            }
-        }
 
     }
 }
